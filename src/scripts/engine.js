6633fce5010be4982b0ad734422548c5dc8b3889
Sergio_Lima <sergiodelima22@gmail.com>
2023-12-13 20:09:03 -0300

    primeiro commit

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,58 +1,77 @@
+// 1 - GUARDANDO OS EMOJIS QUE SERÃO USADOS NOS CARDS (ITENS):
 const emojis = [
-  "🐱",
-  "🐱",
-  "🦝",
-  "🦝",
-  "🦊",
-  "🦊",
-  "🐶",
-  "🐶",
-  "🐵",
-  "🐵",
-  "🦁",
-  "🦁",
-  "🐯",
-  "🐯",
-  "🐮",
-  "🐮",
+    "😀",
+    "😀",
+    "😁",
+    "😁",
+    "😂",
+    "😂",
+    "🤣",
+    "🤣",
+    "😃",
+    "😃",
+    "😄",
+    "😄"
 ];
+// Guardando cartas selecionadas pelo usuário
 let openCards = [];
 
+// Selecionando emojis aleatoriamente:
+    // De alguma forma, isto reforça a aleatoriedade dos emojis selecionados pela máquina.
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
+// Desenhando os elementos na tela:
 for (let i = 0; i < emojis.length; i++) {
-  let box = document.createElement("div");
-  box.className = "item";
-  box.innerHTML = shuffleEmojis[i];
-  box.onclick = handleClick;
-  document.querySelector(".game").appendChild(box);
+    // Criando caixa dinamicamente e associando com elemento com uma tag DIV
+    let box = document.createElement("div");
+    // A classe "item" será associada aqui:
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;   
+    // Pendurando na div principal "game":
+    document.querySelector(".game").appendChild(box);
+    playAudio("soundtrack")
 }
 
+//Tocar música de fundo
+function playAudio(audioName) {
+    const audio = new Audio(`./src/audio/${audioName}.mp3`);
+    audio.volume = 0.2;
+    audio.play();
+}
+
+ // Criando eventos de CLICK:
 function handleClick() {
-  if (openCards.length < 2) {
-    this.classList.add("boxOpen");
-    openCards.push(this);
-  }
+    if(openCards.length < 2) { // Dois porque a comparação é a cada duas cartas.
+        // Sempre que um card for aberto, a classe boxOpen será criada
+        this.classList.add("boxOpen");
+        // Para guardar a carta no vetor
+        openCards.push(this);
+    }
+    // Para comparar se as cartas abertas foram iguais (deu match)
+    if (openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
 
-  if (openCards.length == 2) {
-    setTimeout(checkMatch, 500);
-  }
-
-  console.log(openCards);
 }
-
+//Construindo a condição de pontuação
 function checkMatch() {
-  if (openCards[0].innerHTML === openCards[1].innerHTML) {
-    openCards[0].classList.add("boxMatch");
-    openCards[1].classList.add("boxMatch");
-  } else {
-    openCards[0].classList.remove("boxOpen");
-    openCards[1].classList.remove("boxOpen");
-  }
+    //Se os dois "innerHTML" forem iguais então adicione uma classe no openCard no índice zero chamada classList.
+    if (openCards[0].innerHTML === openCards[1].innerHTML) { //innerHTML é a propriedade que armazena do emoji
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    // caso contrário, é preciso reverter do "openCards" o "boxOpen" que ele atribui ao vetor quando o card é clicado
+    }else{
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
 
-  openCards = [];
+    openCards = [];
 
-  if (document.querySelectorAll(".boxMatch").length === emojis.length) {
-    alert("Você venceu !");
-  }
+    //Colocando condição de finalização do jogo:
+    // Se a quantidade de divs da classe "boxMatch" for igual a quantidade de elementos dentro do vetor "emoji"...
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        //mostre a mensagem abaixo numa caixa de diálogo do browser:
+        alert("Você venceu!");
+    }
 }
